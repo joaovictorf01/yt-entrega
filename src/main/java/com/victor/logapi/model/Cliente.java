d4fd7be627d9935855458ce34034ba286b2bc579
João Victor <joaovictorf01@gmail.comm>
2023-03-23 20:04:35 -0300

    Adicionei a dependência lombok e criei a classe cliente no pacote model.

---
+++ src/main/java/com/victor/logapi/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.victor.logapi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Cliente {
+    private String name;
+    private String email;
+    private String telefone;
+}
